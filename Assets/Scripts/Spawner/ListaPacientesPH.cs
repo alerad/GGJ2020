e09f8b1a5b9991a53f89f403cc7efe251910d40e
unknown <fedisalgado@gmail.com>
2020-02-01 02:25:06 -0300

    Spawner WIP

---
+++ Assets/Scripts/Spawner/ListaPacientesPH.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListaPacientesPH : MonoBehaviour
+{
+    GameObject PatientsPH;
+    public List<GameObject> PatientList; 
+   
+}
