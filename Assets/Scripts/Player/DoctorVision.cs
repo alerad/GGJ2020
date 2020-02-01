79b7318b52844aabf6e25c62e74c66eea04f4137
Ale <aleradisic@gmail.com>
2020-02-01 02:44:57 -0300

    Doctor vision wip

--- Assets/Scripts/Player/DoctorVision.cs
+++ Assets/Scripts/Player/DoctorVision.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UniRx;
 using UnityEngine;
 
 public class DoctorVision : MonoBehaviour {
+   private OVRHand[] hands;
+   
    public ReactiveProperty<bool> inDoctorVision;
 
    private void Start() {
-      Observable.EveryUpdate().Subscribe(x => CheckDoctorVision())
+      hands = new OVRHand[]
+      {
+         GameObject.Find("OVRCameraRig/TrackingSpace/LeftHandAnchor/OVRHandPrefab").GetComponent<OVRHand>(),
+         GameObject.Find("OVRCameraRig/TrackingSpace/RightHandAnchor/OVRHandPrefab").GetComponent<OVRHand>()
+      };
+      
+      Observable.EveryUpdate().Subscribe(x => CheckDoctorVision());
    }
 
    private void CheckDoctorVision() {
-      if (OVRHand.)
+      if (hands.All(x => x.GetFingerIsPinching(OVRHand.HandFinger.Index))
+            && OnVisionTrigger()) {
+                  
+      }
+   }
+
+   private bool OnVisionTrigger() {
+      throw new NotImplementedException();
    }
 }
