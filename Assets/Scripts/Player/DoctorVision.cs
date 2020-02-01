e1b2286cb2a27d16cb861a38a19a9ee8eff97a74
Ale <aleradisic@gmail.com>
2020-02-01 02:16:36 -0300

    Doctor vision

---
+++ Assets/Scripts/Player/DoctorVision.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class DoctorVision : MonoBehaviour {
+   public ReactiveProperty<bool> inDoctorVision;
+
+   private void Start() {
+      Observable.EveryUpdate().Subscribe(x => CheckDoctorVision())
+   }
+
+   private void CheckDoctorVision() {
+      if (OVRHand.)
+   }
+}
