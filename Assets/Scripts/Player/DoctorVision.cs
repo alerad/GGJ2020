d6e789b859d0858ca196dc81156d557e64ce8ebf
Nachoco <ignaciocozza@gmail.com>
2020-02-01 18:04:34 -0300

    Saved scene

---
+++ Assets/Scripts/Player/DoctorVision.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DoctorVision : MonoBehaviour
+{
+
+    public Transform cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickLeft)) {
+            Vector3 pos = cam.transform.localPosition;
+            pos.x -= 0.01f;
+            cam.transform.localPosition = pos;
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickRight)) {
+            Vector3 pos = cam.transform.localPosition;
+            pos.x += 0.01f;
+            cam.transform.localPosition = pos;
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickUp)) {
+            Vector3 pos = cam.transform.localPosition;
+            pos.y += 0.01f;
+            cam.transform.localPosition = pos;
+
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickDown)) {
+            Vector3 pos = cam.transform.localPosition;
+            pos.y -= 0.01f;
+            cam.transform.localPosition = pos;
+        }
+
+
+
+    }
+}
