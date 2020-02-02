b6d597295ecf335e1e3dbda6bec666901e31ec02
Nachoco <ignaciocozza@gmail.com>
2020-02-02 11:36:25 -0300

    Object follower

---
+++ Assets/ObjectFollower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollower : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset = Vector3.zero;
+    
+   
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            transform.position = target.position + offset;
+        }
+        
+    }
+}
