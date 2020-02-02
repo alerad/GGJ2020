7717fca53ba580d3e0ddd8d9b03c3725377f5b3a
Ale <aleradisic@gmail.com>
2020-02-02 12:44:23 -0300

    Test pinch

---
+++ Assets/Scripts/Player/Gestures/ThumbsUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThumbsUp : MonoBehaviour {
+    [SerializeField]
+    private OVRHand handL;
+    [SerializeField]
+    private OVRHand handR;
+
+    public TextMesh text;
+
+    private void Update() {
+        text.text = $"Right hand middlef: {handR.GetFingerPinchStrength(OVRHand.HandFinger.Middle)}\nLeft hand middlef: {handL.GetFingerPinchStrength(OVRHand.HandFinger.Middle)}";
+    }
+}
