31b496b99f9c23ea6a7cea6d921c73964ce1da6d
Nachoco <ignaciocozza@gmail.com>
2020-02-01 22:06:01 -0300

    ingredient Spawner almost finished

---
+++ Assets/Scripts/IngredientSpawner.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MonKey.Extensions;
+using UnityEngine;
+
+public class IngredientSpawner : Singleton <IngredientSpawner>
+{
+
+    public Transform[] spawns;
+    private List<SpawnPoint> spawnPoints;
+    public int extraIngredients;
+
+    class SpawnPoint
+    {
+        public Transform spawn;
+        public Potion pot;
+
+        public SpawnPoint(Transform s)
+        {
+            this.spawn = s;
+        }
+        
+        public bool IsUsed()
+        {
+            return pot != null;
+        }
+    }
+
+    private void Awake()
+    {
+        spawnPoints = new List<SpawnPoint>();
+        spawns.ForEach(s => spawnPoints.Add(new SpawnPoint(s)));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            //SpawnIngredientsForPotion(IngredientMixer.);
+        }
+    }
+
+    public void SpawnIngredientsForPotion(Potion pot)
+    {
+        pot.ingredients.ForEach(SpawnIngredientRandom);
+        List<Potion.Ingredient> availableIngredients = Potion.GetAllIngredients();
+        availableIngredients = availableIngredients.Except(pot.ingredients).ToList();
+        for (int i = 0; i < extraIngredients; i++)
+        {
+            SpawnIngredientRandom(RandomHelper.Get(availableIngredients));
+        }
+    }
+
+    void SpawnIngredientRandom(Potion.Ingredient ingr)
+    {
+        GameObject prefab = null; //Seguir aca
+        Transform s = GetRandomSpawn();
+        Instantiate(prefab, s.position, new Quaternion(), this.transform);
+    }
+
+    Transform GetRandomSpawn()
+    {
+        List<SpawnPoint> availableSpawns = spawnPoints.Where(s => !s.IsUsed()).ToList();
+        return RandomHelper.Get(availableSpawns).spawn;
+    }
+    
+}
